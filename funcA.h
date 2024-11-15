a73e2676660a31bff8092243ef8524f7050c2ba2
YegorKuchyr <144684491+YegorKuchyr@users.noreply.github.com>
2024-11-15 16:19:15 +0200

    Create funcA.h

---
+++ funcA.h
@@ -0,0 +1,13 @@
+#ifndef FUNCA_H
+#define FUNCA_H
+
+//FuncA class contains methods to calculate cosine using Taylor series
+//The parameter n indicates how many terms of the series to compute
+class FuncA {
+	public:
+		double calculateCosine(double x, int n);
+};
+
+double factorial(int num);
+
+#endif
